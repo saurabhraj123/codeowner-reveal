4ff8db14fab313abdb1b714b967a0f1378f0235b
saurabhraj123 <me4saurabh4u@gmail.com>
2025-01-26 20:11:12 +0530

    add codeownwer file support

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,88 @@
 // variables
 let isObserving = false;
 let previousURL = null;
+let trie = null;
+
+// trie
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.codeowner = null;
+  }
+}
+
+class CodeOwnerTrie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(path, codeowner) {
+    const parts = path.split("/");
+    let node = this.root;
+    for (const part of parts) {
+      if (!node.children[part]) {
+        node.children[part] = new TrieNode();
+      }
+      node = node.children[part];
+    }
+    node.codeowner = codeowner;
+  }
+
+  search(filePath) {
+    const parts = filePath.split("/");
+    let node = this.root;
+    let lastCodeowner = null;
+    for (const part of parts) {
+      if (node.children[part]) {
+        node = node.children[part];
+        if (node.codeowner !== null) {
+          lastCodeowner = node.codeowner;
+        }
+      } else {
+        break;
+      }
+    }
+    return lastCodeowner;
+  }
+}
+
+const getCodeownersFileContent = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(["codeowners"], (result) => {
+      if (result.codeowners) {
+        resolve(result.codeowners);
+      } else {
+        reject();
+      }
+    });
+  });
+};
+
+const buildTrie = (codeownersFileContent) => {
+  const codeownersArray = codeownersFileContent
+    .split("\n")
+    .filter((line) => line.length > 0);
+  const trie = new CodeOwnerTrie();
+
+  for (const line of codeownersArray) {
+    const [path, codeowner] = line.split(/\s+/); // assuming space separates path and codeowner
+    if (path && codeowner) {
+      trie.insert(path, codeowner);
+    }
+  }
+
+  return trie;
+};
+
+const getCodeowner = (fileName) => {
+  return trie?.search(fileName);
+};
 
 // mutation observer initialization
-const getElementWithCodeOwner = () => {
+const getElementWithCodeOwner = (filePath = "") => {
   const element = document.createElement("div");
-  element.innerText = "@toddle-edu/frontend-team-3";
+  const codeowner = getCodeowner(filePath);
+  element.innerText = codeowner || "No codeowner found";
   element.classList.add("text-mono", "text-small", "wb-break-all");
   element.dataset.ignoreMutation = "true"; // use this attribute wherever mutation has to be ignored
   return element;
@@ -57,9 +134,10 @@ const handleSummaryPage = (mutations) => {
 
 const updateFileHeader = (node = document) => {
   const headers = node.querySelectorAll(".file-header");
-  headers.forEach((container) => {
-    const fileInfoEle = container.querySelector(".file-info");
-    const element = getElementWithCodeOwner();
+  headers.forEach((header) => {
+    const filePath = header.dataset.path;
+    const fileInfoEle = header.querySelector(".file-info");
+    const element = getElementWithCodeOwner(filePath);
 
     const wrapperDiv = document.createElement("div");
     wrapperDiv.style.display = "flex";
@@ -67,7 +145,7 @@ const updateFileHeader = (node = document) => {
     wrapperDiv.style.flex = 1;
 
     if (fileInfoEle) {
-      container.replaceChild(wrapperDiv, fileInfoEle);
+      header.replaceChild(wrapperDiv, fileInfoEle);
       wrapperDiv.appendChild(fileInfoEle);
       wrapperDiv.appendChild(element);
     }
@@ -115,8 +193,7 @@ const startObserving = (message) => {
   const url = message.url;
   const isComparePage = url.includes("/compare/");
   const isSummaryPage = url.split("/").slice(-2, -1)[0] === "pull";
-  const isFilesPage =
-    url.includes("/pull/") && url.split("/").pop().startsWith("files");
+  const isFilesPage = url.includes("/pull/") && url.includes("files");
 
   if (!isComparePage && !isFilesPage && !isSummaryPage) return;
 
@@ -160,3 +237,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     stopObserving();
   }
 });
+
+window.onload = async () => {
+  try {
+    const codeownersFileContent = await getCodeownersFileContent();
+
+    if (codeownersFileContent) {
+      trie = buildTrie(codeownersFileContent);
+    }
+  } catch (err) {
+    console.log("Error while loading codeowners file.");
+  }
+};
